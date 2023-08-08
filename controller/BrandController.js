ff29f5f5a623a22739a3f24e5724c1c4a42b597d
faiz-webdev <faijuddin@tothenew.com>
2023-08-08 16:19:40 +0530

    5. check duplicate brand

--- controller/BrandController.js
+++ controller/BrandController.js
@@ -1,14 +1,19 @@
 const { Brand } = require("../model/Brand");
+const isEmpty = require("lodash.isempty");
 
 exports.createBrand = async (req, res) => {
   const brand = new Brand(req.body);
   try {
-    let getBrand = await Brand.find({});
+    const { label, value } = req.body;
+    let getBrand = await Brand.findOne({
+      $or: [{ label: label }, { value: value }],
+    });
+
     if (getBrand) {
       res.status(400).json({ message: "Brand already taken", data: {} });
     } else {
       const doc = await brand.save();
-      res.status(201).json({ data: brand });
+      res.status(201).json({ data: doc, message: "brand created" });
     }
   } catch (err) {
     res.status(400).json(err);
@@ -18,7 +23,7 @@ exports.createBrand = async (req, res) => {
 exports.getBrand = async (req, res) => {
   try {
     const brands = await Brand.find({});
-    res.status(201).json({ brands });
+    res.status(201).json({ message: "brand found", brands });
   } catch (err) {
     res.status(400).json(err);
   }
